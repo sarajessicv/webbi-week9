4f2caa48e62989caa63237ce781014a4b6dee10d
sarajessicv <sara.j.heikkinen@gmail.com>
2022-07-15 14:29:55 +0300

    1&2

--- src/components/MyContainer.js
+++ src/components/MyContainer.js
@@ -5,7 +5,12 @@ const MyContainer = () => {
   return (
     <div>
       <h2>My Container</h2>
-      <MyList />
+      <MyList header="Really epic list component"
+      items={[
+        { id: "1", text: "This is an item" },
+        { id: "2", text: "Also this" },
+      ]} 
+      />
     </div>
   )
 }
--- src/components/MyList.js
+++ src/components/MyList.js
@@ -1,8 +1,15 @@
 
-function MyList() {
+function MyList({header, items}) {
+
+    const listItems = items.map((element) => (
+        <li key={element.id}>{element.text}</li>
+    ))
+    
   return (
     <div>
-      <h3>My List</h3>
+      <h3>My List </h3>
+      <h1>{header}</h1>
+      <ol>{listItems}</ol>
     </div>
   )
 }
