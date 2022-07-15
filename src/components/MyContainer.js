44b8a8e17ac98dcc2a57a8e4a8dbebc06bf8278a
sarajessicv <sara.j.heikkinen@gmail.com>
2022-07-15 15:46:18 +0300

    1-4

--- src/components/MyContainer.js
+++ src/components/MyContainer.js
@@ -1,15 +1,32 @@
+import { useState } from 'react'
 import MyList from './MyList.js'
 
 
 const MyContainer = () => {
+
+    const [text, setText] = useState('')
+
+    const [items, setItems] = useState([
+        { id: "1", text: "This is an item" },
+        { id: "2", text: "Also this" },
+    ]);
+
+    const addList = () => {
+        const id = items.length + 1;      
+        const newItem = {id, text}; 
+        setItems([...items, newItem]);
+        setText('')
+
+    }
+
   return (
     <div>
       <h2>My Container</h2>
+      <textarea id="textArea" placeholder='Add new list item' onChange={(e) => setText(e.target.value)} value={text}></textarea>
+      <button id="addButton" onClick={addList}>Add new item</button>
+
       <MyList header="Really epic list component"
-      items={[
-        { id: "1", text: "This is an item" },
-        { id: "2", text: "Also this" },
-      ]} 
+      items={items} 
       />
     </div>
   )
